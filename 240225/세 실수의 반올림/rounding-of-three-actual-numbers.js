39944d294ef0d81253e5613ad049d86dfdeee392
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-02-25 17:22:20 +0900

    Title: 세 실수의 반올림, Time: 228ms, Memory: 14MB, Status: Passed - CodeTree

--- 240225/세 실수의 반올림/rounding-of-three-actual-numbers.js
+++ 240225/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().split('\n')
-input = input.map(Number);
+input = input.map(x => Number(x).toFixed(3));
 
-for(let i = 0; i < input.length; i++){
-    console.log(input[i].toFixed(3))
-}
+console.log(input[0])
+console.log(input[1])
+console.log(input[2])
