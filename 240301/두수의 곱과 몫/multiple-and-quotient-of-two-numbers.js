aec616c8e529e374a2c139d77c16a9c5162af7bc
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-01 15:52:39 +0900

    Title: 두수의 곱과 몫, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240301/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split(' ').map(Number);
+let a = input[0]
+let b = input[1]
+
+console.log(`${a} * ${b} = ${a * b}`)
+console.log(`${a} / ${b} = ${parseInt(a / b)}`)
