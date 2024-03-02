28bc904564bc0359d2947c37a5676c08862d7fa9
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-02 16:13:54 +0900

    Title: 큰 수에서 빼기, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240302/큰 수에서 빼기/subtract-from-large-number.js
+++ 240302/큰 수에서 빼기/subtract-from-large-number.js
@@ -4,10 +4,8 @@ const input = fs.readFileSync(0).toString().split(' ').map(Number);
 let a = input[0]
 let b = input[1]
 
-if(a > b){
+if(a > b || a === b){
     console.log(a - b)
 }else if(a < b){
     console.log(b - a)
-}else{
-    console.log(a)
 }
