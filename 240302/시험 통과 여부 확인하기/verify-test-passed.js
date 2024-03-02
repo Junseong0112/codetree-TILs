a8ab50d44de96deeed0f2d27550a7a834adb7fc9
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-02 16:42:45 +0900

    Title: 시험 통과 여부 확인하기, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240302/시험 통과 여부 확인하기/verify-test-passed.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString();
+
+let n = parseInt(input)
+
+if(n > 80){
+    console.log('pass')
+}else{
+    console.log(`${80 - n} more score`)
+}
