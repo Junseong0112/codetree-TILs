9fceb49b3d9cb22968681c1ec9b572cf9547e43a
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-02 15:48:30 +0900

    Title: 합과 평균, Time: 221ms, Memory: 14MB, Status: Passed - CodeTree

--- 240302/합과 평균/sum-and-avg.js
+++ 240302/합과 평균/sum-and-avg.js
@@ -4,4 +4,4 @@ const input = fs.readFileSync(0).toString().split(' ').map(Number)
 let a = input[0]
 let b = input[1]
 
-console.log(a + b, (a + b) / 2)
+console.log(a + b, ((a + b) / 2).toFixed(1))
