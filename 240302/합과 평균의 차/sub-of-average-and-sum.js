b25b21d980f1e400ffbbea0533c8005510e4c40c
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-02 15:51:46 +0900

    Title: 합과 평균의 차, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240302/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().split(' ').map(Number);
+
+let a = input[0];
+let b = input[1];
+let c = input[2];
+
+console.log(a + b + c);
+console.log((a + b + c) / 3);
+console.log((a + b + c) - ((a + b + c) / 3))
