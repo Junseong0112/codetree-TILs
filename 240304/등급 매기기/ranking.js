cc7910c846343e8f99ca93855dd1feeb9d565d25
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-04 15:55:01 +0900

    Title: 등급 매기기, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240304/등급 매기기/ranking.js
+++ 240304/등급 매기기/ranking.js
@@ -5,11 +5,11 @@ let n = Number(input);
 
 if(n >= 90){
     console.log('A')
-}else if(n > 89 && n >= 80){
+}else if(n > 90 && n >= 80){
     console.log('B')
-}else if(n > 79 && n >= 70){
+}else if(n > 80 && n >= 70){
     console.log('C')
-}else if(n > 69 && n >= 60){
+}else if(n > 70 && n >= 60){
     console.log('D')
 }else{
     console.log('F')
