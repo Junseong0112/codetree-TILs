8211335a92322614e8eb3ac38559a4f886326b3e
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-04 15:59:18 +0900

    Title: 알파벳에 따른 평가, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240304/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 240304/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString();
 
-if(input === 'S') console.log('Superior');
-else if(input === 'A') console.log('Excellent');
-else if(input === 'B') console.log('Good');
-else if(input === 'C') console.log('Usually');
-else if(input === 'D') console.log('Effort');
-else console.log('Failure')
+if(input === 'S') {console.log('Superior')}
+else if(input === 'A') {console.log('Excellent')}
+else if(input === 'B') {console.log('Good')}
+else if(input === 'C') {console.log('Usually')}
+else if(input === 'D') {console.log('Effort')}
+else {console.log('Failure')}
