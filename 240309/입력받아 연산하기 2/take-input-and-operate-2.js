0e8301e230c5751ab2b1652d41cbaa143c2a2240
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-09 18:42:18 +0900

    Title: 입력받아 연산하기 2, Time: 199ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240309/입력받아 연산하기 2/take-input-and-operate-2.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+let a = input[0];
+let b = input[1];
+
+console.log(a + 894);
+console.log(parseInt(b / 10))
