5030b719ead5f7815ab08fe35f99ab0842bfbd43
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-03-16 20:27:15 +0900

    Title: 점수 비교 2, Time: 212ms, Memory: 47MB, Status: Passed - CodeTree

--- 240316/점수 비교 2/score-comparison-2.js
+++ 240316/점수 비교 2/score-comparison-2.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let a = input[0].split(' ').map(Number)
 let b = input[1].split(' ').map(Number)
 
-if(a[0] > a[1] && b[0] > b[1]){
+if(a[0] > b[0] && a[1] > b[1]){
     console.log(1)
 }else{
     console.log(0)
