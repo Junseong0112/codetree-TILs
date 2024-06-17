c8d0f6bb5275dd0b86d512639f7a0da78f57921b
Junseong <108931006+Junseong0112@users.noreply.github.com>
2024-06-17 21:04:05 +0900

    Title: 십진수와 이진수, Time: 236ms, Memory: 47MB, Status: Passed - CodeTree

--- 240617/십진수와 이진수/decimal-and-binary-number.js
+++ 240617/십진수와 이진수/decimal-and-binary-number.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim()
 
-let seventeen = parseInt(input, 2) * 17
+// 입력된 이진수를 BigInt로 변환하고 17을 곱함
+let seventeen = BigInt('0b' + input) * 17n
 
+// 결과를 이진수 문자열로 변환하여 출력
 console.log(seventeen.toString(2))
